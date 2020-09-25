0fccfc8005c6e007a4eaa554c4f26ca89bf22417
Leander Kunstmann <leander.kunstmann@gmx.de>
2020-09-25 20:10:26 +0200

    comments

--- src/components/vminfo.js
+++ src/components/vminfo.js
@@ -1,14 +1,14 @@
 const vm_info = [
-    ['HAL91', 'Username','Passwort','tooltip'],
-    ['Oberon', 'Username','Passwort','tooltip'],
-    ['OpenBSD 6.5 (Fvwm)','Username','Passwort','tooltip'],
-    ['OPENSTEP 4.2','Username','Passwort','tooltip'],
-    ['OS2 1.30 (Microsoft)','Username','Passwort','tooltip'],
-    ['OS2-W4','ReactOS 0.4.9','Username','Passwort','tooltip'],
+    ['HAL91', 'nicht benötigt','',''],
+    ['OpenBSD 6.5 (Fvwm)','root','BSDtoor','tooltip'],
+    ['OPENSTEP 4.2','nicht benötigt','','tooltip'],
+    ['OS2 1.30 (Microsoft)','nicht benötigt','','tooltip'],
+    ['OS2-W4','nicht benötigt','','tooltip'],
+    ['ReactOS 0.4.9','nicht benötigt','','tooltip'],
     ['sol-11_4-vbox','Username','Passwort','tooltip'],
     ['TrueOS 18.12 stable (Mate)','root','TRUEtoor','tooltip'],
     ['Unix System V R4','Username','Passwort','tooltip'],
-    ['Win NT 3.51','Username','Passwort',''],
+    ['Win NT 3.51','Administrator','admin',''],
     ['Win NT 4 (clean)','Administrator','admin',''],
     ['WIN3.1 (SND, SVGA, NET)','Administrator','admin','tooltip'],
     ['Xenix 386 2.3.4q','Username','Passwort','tooltip'],
@@ -21,4 +21,36 @@ const vm_info = [
 ];
 
 export default vm_info;
+
+//
+//openbsd mausbug
+//openstep ultimativer mausbug
+
+//maus geht gar nicht bei os2 1.3
+//os w4 maus ist ok aber shit
+//React os sehr nice
+//mate vbox anschauen und fixen
+//win nt 3.51 maus
+//win nt 4 mit rdp perfekt, vnc web trash, nur normales vnc
+//win 3.1 network delete
+//cpm nice
+//dos 3 mouse
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
